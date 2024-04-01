9475c1cc2b9fb40f57b977d1f19e1725f8112f75
Angelo <brasiliafan@gmail.com>
2024-04-01 10:53:39 +0200

    creation de la classe Humain et modification de la classe Femme et Homme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -2,6 +2,10 @@ package iut.bad;
 
 public class Femme extends Humain {
 	
+	private String nom;
+	private String prenom;
+	private int age;
+	
 	public Femme() {
 		super();
 	}
--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -2,9 +2,4 @@ package iut.bad;
 
 public class Humain {
 	
-	private String nom;
-	private String prenom;
-	private int age;
-	
-
 }
