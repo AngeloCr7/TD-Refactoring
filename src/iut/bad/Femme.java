9de602c4a770c610dae6fcd855d56ad8dafff0c9
Angelo <brasiliafan@gmail.com>
2024-04-01 10:57:24 +0200

    deplacement des attributs Homme/Femme dans la classe parente

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -2,10 +2,6 @@ package iut.bad;
 
 public class Femme extends Humain {
 	
-	private String nom;
-	private String prenom;
-	private int age;
-	
 	public Femme() {
 		super();
 	}
--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -2,10 +2,6 @@ package iut.bad;
 
 public class Homme extends Humain {
 	 
-	private String nom;
-	private String prenom;
-	private int age;
-	
 	public Homme() {
 		super();
 	}
